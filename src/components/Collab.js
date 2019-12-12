ac843ca56f356745b57b63e49c94d4f87a48f32a
cieniavska <katarzyna.cieniawska@gmail.com>
2019-12-12 16:36:38 +0100

    json work

--- src/components/Collab.js
+++ src/components/Collab.js
@@ -1,37 +1,51 @@
 import React, {Component} from 'react';
 import SectionTitle from './../components/Reusables/SectionTitle';
+import organizations from './../data/organizations';
+
 
 export default class Collab extends Component {
 
-    stan = {
-        activeView: 1
+    state = {
+        activeView: 0
     }
 
     changeView = (viewNum) => {
         console.log(viewNum);
         this.setState({
             activeView: viewNum
-        })
+        });
+        this.showIndexDescription();
+    }
+
+    showIndexDescription = () => {
+        if (this.state.activeView === "0") {
+            return 0;
+        } else if (this.state.activeView === "1") {
+            return 1;
+        } else {
+            return 2;
+        }
     }
 
     render() {
+        
         return (
             <div>
                     <div name="collab" className="collab__container flex">
                     <SectionTitle title="Komu pomagamy?"/>
                         <div className="collab__container__partners">
+                            <button className="collab__container__partner" onClick={() => this.changeView(0)}>
+                                <span>{organizations.organizations[0].navName}</span>
+                            </button>
                             <button className="collab__container__partner" onClick={() => this.changeView(1)}>
-                                <span>Fundacjom</span>
+                                <span>{organizations.organizations[1].navName}</span>
                             </button>
                             <button className="collab__container__partner" onClick={() => this.changeView(2)}>
-                                <span>Organizacjom pozarządowym</span>
-                            </button>
-                            <button className="collab__container__partner" onClick={() => this.changeView(3)}>
-                                <span>Lokalnym zbiórkom</span>
+                                <span>{organizations.organizations[2].navName}</span>
                             </button>
                         </div>
                         <div>
-                            <span>Lorem ipsum dolor sit amet, tempor incididunt. Ut exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.</span>
+                            <span>{organizations.organizations[this.state.activeView].description}</span>
                         </div>
                     </div>
             </div>
