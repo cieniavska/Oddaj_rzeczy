7c7f26c254cf085f8d4d1285ec764c3ec8f6c4a0
cieniavska <katarzyna.cieniawska@gmail.com>
2019-12-12 18:30:16 +0100

    json work

--- src/components/Collab.js
+++ src/components/Collab.js
@@ -6,13 +6,32 @@ import organizations from './../data/organizations';
 export default class Collab extends Component {
 
     state = {
-        activeView: 0
+        activeView: 0,
+        availableButtons: 2,
+        activePage: 1
     }
 
     changeView = (viewNum) => {
-        console.log(viewNum);
+
+        console.log(
+            organizations.organizations[0].foundations.length,
+            organizations.organizations[1].foundations.length,
+            organizations.organizations[2].foundations.length,
+            );
+
+            let howManyButtons;
+            let numberOfPositions = parseInt(organizations.organizations[this.state.activeView].foundations.length);
+            if (numberOfPositions % 3 === 0) {
+                howManyButtons = numberOfPositions / 3;
+            } else if (numberOfPositions === 3) {
+                howManyButtons = 1;
+            } else {
+                howManyButtons = 0;
+            }
+
         this.setState({
-            activeView: viewNum
+            activeView: viewNum,
+            availableButtons: howManyButtons 
         });
         this.showIndexDescription();
     }
@@ -27,7 +46,18 @@ export default class Collab extends Component {
         }
     }
 
+    displayButtons = () => {
+        let i = 0;
+        let buttons = [];
+        while (i < this.state.availableButtons) {
+          i++;
+          buttons.push(<button className="collab__container__button">{i}</button>);
+        }
+        return buttons;
+      }
+
     render() {
+        
         
         return (
             <div>
@@ -46,6 +76,17 @@ export default class Collab extends Component {
                         </div>
                         <div>
                             <span>{organizations.organizations[this.state.activeView].description}</span>
+                        </div>
+                        <div className="collab__container__list flex">
+                            <div className="collab__container__list__first-box">
+                                <div className="collab__container__list__first-box__title">{organizations.organizations[0].foundations[0].name}</div>
+                                <div className="collab__container__list__first-box__description">{organizations.organizations[0].foundations[0].mission}</div>
+                            </div>
+                            <div className="collab__container__list__second-box">{organizations.organizations[0].foundations[0].things}</div>
+                        </div>
+                        <div className="collab__container__buttons">
+                            
+                            {this.displayButtons()}
                         </div>
                     </div>
             </div>
